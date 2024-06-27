a52f0cc4540551024c5870973b65d8a254a53e43
shreyas <hello@shreyas.im>
2024-06-27 13:39:22 +0530

    feat: add and setup builder react sdk

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,11 +1,43 @@
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+// ($slug)._index.tsx
+import {
+  Content,
+  fetchOneEntry,
+  getBuilderSearchParams,
+  isPreviewing,
+} from '@builder.io/sdk-react';
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+import { fetch as webFetch } from '@remix-run/web-fetch';
 
-export async function loader({request}: LoaderFunctionArgs) {
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
+const apiKey = "34490c3c59c34f3db7d5529a389a155c";
+
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const urlPath = `/${params['slug'] || ''}`;
+
+  const page = await fetchOneEntry({
+    model: 'page',
+    apiKey: apiKey,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: { urlPath },
+    fetch: webFetch,
   });
-}
 
-export default function CatchAllPage() {
-  return null;
+  if (!page && !isPreviewing(url.search)) {
+    throw new Response('Page Not Found', {
+      status: 404,
+      statusText: 'Page not found in Builder.io',
+    });
+  }
+
+  return { page };
+};
+
+// Define and render the page.
+export default function Page() {
+  // Use the useLoaderData hook to get the Page data from `loader` above.
+  const { page } = useLoaderData<typeof loader>();
+
+  // Render the page content from Builder.io
+  return <Content model="page" apiKey={apiKey} content={page as any} />;
 }
