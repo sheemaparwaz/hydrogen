39bddbf3c3527adde793bdd47248dd24a6783f15
sheemaparwaz <sheemaparwaz@gmail.com>
2024-07-03 21:01:06 +0530

    first commit

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -5,21 +5,21 @@ import {
   getBuilderSearchParams,
   isPreviewing,
 } from '@builder.io/sdk-react';
-import type { LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
-import { fetch as webFetch } from '@remix-run/web-fetch';
+import type {LoaderFunctionArgs} from '@remix-run/node';
+import {useLoaderData} from '@remix-run/react';
+import {fetch as webFetch} from '@remix-run/web-fetch';
 
-const apiKey = "34490c3c59c34f3db7d5529a389a155c";
+const apiKey = 'bb81a0bca7f54989879fda4341f85c31';
 
-export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+export const loader = async ({params, request}: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const urlPath = `/${params['slug'] || ''}`;
 
   const page = await fetchOneEntry({
     model: 'page',
-    apiKey: apiKey,
+    apiKey,
     options: getBuilderSearchParams(url.searchParams),
-    userAttributes: { urlPath },
+    userAttributes: {urlPath},
     fetch: webFetch,
   });
 
@@ -30,13 +30,13 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
     });
   }
 
-  return { page };
+  return {page};
 };
 
 // Define and render the page.
 export default function Page() {
   // Use the useLoaderData hook to get the Page data from `loader` above.
-  const { page } = useLoaderData<typeof loader>();
+  const {page} = useLoaderData<typeof loader>();
 
   // Render the page content from Builder.io
   return <Content model="page" apiKey={apiKey} content={page as any} />;
